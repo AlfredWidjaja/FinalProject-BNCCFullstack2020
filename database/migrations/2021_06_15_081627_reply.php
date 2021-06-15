47ee0b391d3a128daaded9a856b9aeec80e414f1
Alfred Widjaja <alfredwidjaja01@gmail.com>
2021-06-15 15:26:10 +0700

    Database migration for final project

---
+++ database/migrations/2021_06_15_081627_reply.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Reply extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reply', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->unique();
+            $table->string('description');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->unsignedBigInteger('thread_id');
+            $table->foreign('thread_id')->references('id')->on('thread');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reply');
+    }
+}
